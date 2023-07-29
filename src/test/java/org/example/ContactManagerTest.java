d452b9462c4954881b107740dcc24b78e48ee06d
kashaf_ashraf230 <kashafashraf412@gmail.com>
2023-07-29 18:46:13 +0500

    First Junit Test

--- src/test/java/org/example/ContactManagerTest.java
+++ src/test/java/org/example/ContactManagerTest.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,6 +11,14 @@ class ContactManagerTest {
 
     @BeforeEach
     void setUp() {
+    }
+
+    @Test
+    public void shouldCreateContact(){
+        ContactManager contactManager = new ContactManager();
+        contactManager.addContact("Kashaf", "Ashraf", "03048888909");
+        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+
     }
 
     @AfterEach
